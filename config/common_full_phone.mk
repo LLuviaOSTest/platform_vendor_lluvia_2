f244b661c5a336625267bee9d9a021a95f131b23
cordell <cordellsmail@gmail.com>
2017-09-02 11:17:48 +0100

    vendor: World phone support
    - Kanged latest apns-conf.xml from LineageOS
    
    Change-Id: Ibfc2ba0d58dd55f01d43435817e0cedf6f05d6bd
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,10 @@
 # Inherit common stuff
 $(call inherit-product, vendor/yodita/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/yodita/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     vendor/yodita/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
