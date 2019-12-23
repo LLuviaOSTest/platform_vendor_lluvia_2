ArrowOS/android_vendor_arrow

vendor: Allow AOSP KB to gesture type
- Add condition for 32/64 devices

Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec212

config/common.mk
@@ -55,6 +55,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/arrow/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/arrow/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
+
 # Pixel sysconfig
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml

LLuviaOSTest/platform_vendor_lluvia_2

Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

config/common.mk
@@ -26,6 +26,17 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/yodita/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/yodita/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/yodita/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/yodita/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/yodita/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/yodita/prebuilt/common/etc/mkshrc:system/etc/mkshrc